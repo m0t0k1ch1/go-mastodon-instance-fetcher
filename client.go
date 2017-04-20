da65638429fb72247d87221b0108a586114dbe2e
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2017-04-20 19:58:03 +0900

    Instance.HttpScore: float64

--- client.go
+++ client.go
@@ -29,7 +29,7 @@ type Instance struct {
 	Name              string  `json:"name"`
 	Uptime            float64 `json:"uptime"`
 	Up                bool    `json:"up"`
-	HttpsScore        int     `json:"https_score"`
+	HttpsScore        float64 `json:"https_score"`
 	HttpsRank         string  `json:"https_rank"`
 	IPv6              bool    `json:"ipv6"`
 	OpenRegistrations bool    `json:"openRegistrations"`
